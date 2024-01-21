4a72b2b28c6f40cec08db1a9c26f0d7b13909e82
Gerry Pass <rgpass@gmail.com>
2024-01-21 14:35:47 -0500

    Fix brew to add command to PATH

--- install.sh
+++ install.sh
@@ -70,6 +70,8 @@ fi
 if test ! $(which brew); then
   echo "> Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "> Updating Homebrew..."
   brew update
