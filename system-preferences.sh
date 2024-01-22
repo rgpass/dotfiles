565f11bc0fb95c7b3a0950655cf8fde76ba5296a
Gerry Pass <rgpass@gmail.com>
2024-01-21 21:14:46 -0500

    Organize Raycast notes better

--- system-preferences.sh
+++ system-preferences.sh
@@ -86,11 +86,17 @@ echo "> Some Mac OSX preferences must be set manually. Follow these steps..."
 
 echo "> System Preferences > Displays > Refresh rate > 60 Hertz"
 
+###############################################################################
+# Raycast                                                                     #
+###############################################################################
+
 echo "> Steps to set up Raycast:"
 echo "> 1. Open Spotlight (Cmd+Space) and open Raycast"
 echo "> 2. Follow setup wizard"
 echo "> 3. System Preferences > Keyboard > Shortcuts > Spotlight > uncheck both"
 echo "> 4. Raycast (click icon in menu bar) > Settings > Raycast Hotkey > Cmd+Space"
+echo ">"
+echo "> After Raycast setup complete, configure snippets"
 
 ###############################################################################
 # Kill affected applications                                                  #
